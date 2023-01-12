d8822864938c5a8c058bdc0f27925193715d9254
Vinicius de Morais Santos <vinicius.morais@mercadolivre.com>
2023-01-12 15:30:17 -0300

    Lacos Repeticao JAVA 120123

--- Main.java
+++ Main.java
@@ -3,86 +3,89 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner ler = new Scanner(System.in);
+        int numero, totalDeNumerosPares, totalDeNumeroImpares;
 
-        int numero;
+        Scanner leia = new Scanner(System.in);
+        totalDeNumeroImpares = 0;
+        totalDeNumerosPares = 0;
 
-        System.out.println("Digite um número: ");
-        numero = ler.nextInt ();
+        System.out.println ("Digite números aleatórios para saber quantos são pares ou impares: ");
 
-        if (numero % 2 == 0 && numero > 0)
-            System.out.println ("O número " + numero + " é par e positivo!");
+        for (int contadorNumero = 1; contadorNumero<=10; contadorNumero++){
+            System.out.println("\n Digite o " + contadorNumero + "º número: ");
+            numero = leia.nextInt();
 
-        else if (numero % 2 != 0 && numero > 0)
-            System.out.println ("O número "+ numero + " é ímpar e positivo!");
+            if (numero % 2 == 0)
+                totalDeNumerosPares ++;
+            else
+                totalDeNumeroImpares ++;
 
-        else if (numero % 2 == 0 && numero < 0)
-            System.out.println ("O número " + numero + " é par e negativo");
+        }
 
-        else if (numero % 2 != 0 && numero < 0)
-            System.out.println ("O número "+ numero + " é ímpar e negativo!");
+        System.out.println ("Total de números pares: " + totalDeNumerosPares);
+        System.out.println ("\nTotal de números ímares:" + totalDeNumeroImpares);
 
-        else System.out.println ("zero");
+
 
 
     }
 }
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Scanner ler = new Scanner(System.in);
+        Scanner leia = new Scanner(System.in);
+        int numero;
+        int soma = 0;
 
-        int codigoDoProduto, quantidade, produto, valorTotal;
+        do{
+            System.out.println ("Digite um número: ");
+            numero = leia.nextInt();
+            if (numero > 0)
+                soma += numero;
+            else if (numero < 0) System.out.println("Esse número não será somado");
 
-        System.out.println ("# ## ### #### #########   MENU   ########## #### ### ## #\n");
+            else System.out.println("Número nulo");
 
-        System.out.println ("1 Cachorro-quente - R$ 10,00\n");
-        System.out.println ("2 X-Salada - R$ 15,00\n");
-        System.out.println ("3 X-Bacon - R$ 18,00\n");
-        System.out.println ("4 Bauru - R$ 12,00\n");
-        System.out.println ("5 Refrigerante - R$ 8,00\n");
-        System.out.println ("6 Suco de laranja - R$ 13,00\n");
-
-        System.out.println ("Digite aqui o número do seu pedido\n");
-        codigoDoProduto = ler.nextInt();
-
-        if (codigoDoProduto > 6 || codigoDoProduto < 1)
-            System.out.println ("Digite um número entre 1 e 6.");
-        switch (codigoDoProduto)
-        {
-            case 1:
-                System.out.println ("Quantidade?: ");
-                quantidade = ler.nextInt();
-                System.out.println ("Produto: Cachorro-Quente\n" + "Valor Total R$ " + (quantidade * 10));
-                break;
-            case 2:
-                System.out.println ("Quantidade?: ");
-                quantidade = ler.nextInt();
-                System.out.println ("Produto: X-Salada\n" + "Valor Total R$ " + (quantidade * 15));
-                break;
-            case 3:
-                System.out.println ("Quantidade?: ");
-                quantidade = ler.nextInt();
-                System.out.println ("Produto: X-Bacon\n" + "Valor Total R$ " + (quantidade * 18));
-                break;
-            case 4:
-                System.out.println ("Quantidade?: ");
-                quantidade = ler.nextInt();
-                System.out.println ("Produto: Bauru\n" + "Valor Total R$ " + (quantidade * 12));
-                break;
-            case 5:
-                System.out.println ("Quantidade?: ");
-                quantidade = ler.nextInt();
-                System.out.println ("Produto: Refrigerante\n" + "Valor Total R$ " + (quantidade * 8));
-                break;
-            case 6:
-                System.out.println ("Quantidade?: ");
-                quantidade = ler.nextInt();
-                System.out.println ("Produto: Suco de laranja\n" + "Valor Total R$ " + (quantidade * 13));
-                break;
         }
+
+        while (numero !=  0);
+        System.out.println ("\nDescubra a soma dos números positivos: " + soma);
+
+
+
+
+    }
+}
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 0;
+        int somaIdadeMaior = 0;
+        int somaIdadeMenor = 0;
+
+        Scanner leia = new Scanner(System.in);
+
+
+        while (idade >= 0) {
+            System.out.println("Digite sua idade: ");
+            idade = leia.nextInt();
+
+            if (idade < 21 && idade > 0) somaIdadeMenor++;
+            else if (idade > 50) somaIdadeMaior ++;
+
+        }
+        System.out.println("Total de pessoas menores de 21 anos: " + somaIdadeMenor);
+        System.out.println("Total de pessoas maiores de 50 anos: " + somaIdadeMaior);
+
+
 
     }
 }
